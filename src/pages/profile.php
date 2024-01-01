0243d44928b3296e3da9061eb302d16d4f96ed20
Oicanji <51388779+Oicanji@users.noreply.github.com>
2024-01-01 13:25:45 -0300

    fix: profile page

--- src/pages/profile.php
+++ src/pages/profile.php
@@ -34,28 +34,24 @@
                     </div>
                 </div>
             </div>
-            <div class="col-8 col-xl-9 p-3">
+            <div class="co-sm-12 col-md-8 col-xl-9 p-3">
                 <div class="h-100 rounded border border-3 border-white p-2 border-opacity-10">    
                     <p class="text-primary">
                         Minhas aptidões principais:
                     </p>
                     <hr>
-                    <br />
-                    <h5 class="text-start ms-2">
-                        Aptidões em desenvolvimento frontend:
-                    </h5>
-                    <div class="grid-box" style="justify-content: left;">
+                    <div class="grid-box" style="justify-content: center;">
                         <div style="color: #ffdc2b;">
                             <i class="fa-brands fa-js"></i>
-                            <p>Java Script</p>
+                            <p>JavaScript</p>
                         </div>
                         <div>
                             <i class="fa-solid fa-quote-left"></i>
-                            <p>Type Script</p>
+                            <p>TypeScript</p>
                         </div>
                         <div style="color: #2bedff;">
                             <i class="fa-brands fa-react"></i>
-                            <p4>React</p4>
+                            <p>React</p>
                         </div>
                         <div>
                             <i class="fa-brands fa-bootstrap"></i>
@@ -63,147 +59,141 @@
                         </div>
                         <div style="color: #f77ee5;">
                             <i class="fa-brands fa-sass"></i>
-                            <p4>Sass</p4>
+                            <p>Sass</p>
                         </div>
-                    </div>
-                    <h5 class="text-start ms-2 mt-3">
-                        Aptidões em desenvolvimento backend:
-                    </h5>
-                    <div class="grid-box" style="justify-content: left;">
                         <div>
                             <i class="fa-brands fa-python"></i>
                             <h5>Python</h5>
                         </div>
                         <div style="color: #8e9dfa;">
                             <i class="fa-brands fa-php"></i>
-                            <p4>PHP</p4>
+                            <p>PHP</p>
                         </div>
                         <div>
                             <i class="fa-solid fa-flask"></i>
-                            <p4>Flask</p4>
+                            <p>Flask</p>
                         </div>
                         <div class="text-secondary">
                             <i class="fa-solid fa-database"></i>
-                            <p4>SQL</p4>
+                            <p>SQL</p>
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
-        <div class="rounded border border-3 border-white p-2 border-opacity-10">
-            <p3 class="text-primary">
-                Outras aptidões:
-            </p3>
-            <br />
-            <div class="grid-box">
-                <div class="text-success">
-                    <i class="fa-brands fa-node"></i>
-                    <p>Node</p>
-                </div>
-                <div style="color: #905beb;">
-                    <i class="fa-brands fa-kickstarter-k"></i>
-                    <p>Kotlin</p>
-                </div>
-                <div>
-                    <i class="fa-solid fa-mobile-screen"></i>
-                    <p>Android Studio</p>
-                </div>
-                <div class="unity">
-                    <i class="fa-brands fa-unity"></i>
-                    <p>Unity (C#)</p>
-                </div>
-                <div>
-                    <i class="fa-solid fa-panorama"></i>
-                    <p>Photoshop</p>
-                </div>
-                <div>
-                    <i class="fa-solid fa-film"></i>
-                    <p>Sony Vegas</p>
-                </div>
-                <div>
-                    <i class="fa-regular fa-face-grin-wide"></i>
-                    <p>Desenho Anime</p>
-                </div>
-            </div>
-            <br />
-            <hr />
-            <br />
-            <p3 class="text-primary">
-                Aptidões Específicas ou pessoais:
-            </p3>
-            <br />
-            <div class="grid-box">
-                <div class="text-danger">
-                    <i class="fa-solid fa-s"></i>
-                    <p><span class="text-primary">Es</span><span class="text-white">p</span><span class="text-primary">an</span><span class="text-white">hol</span> Fluente</p>
-                </div>
-                <div class="text-info">
-                    <i class="fa-solid fa-flag-usa"></i>
-                    <p>Inglês Avançado</p>
-                </div>
-                <div>
-                    <i class="fa-brands fa-java"></i>
-                    <p4>Java</p4>
-                </div>
-                <div class="text-warning">
-                    <i class="fa-solid fa-bolt"></i>
-                    <p4>Vite</p4>
-                </div>
-                <div>
-                    <i class="fa-solid fa-hard-drive"></i>
-                    <p style="font-size:12px">Manutenção M. Computadores</p>
-                </div>
-                <div>
-                    <i class="fa-solid fa-person-chalkboard"></i>
-                    <p>Monitor de Programação</p>
-                </div>
-                <div>
-                    <i class="fa-solid fa-tree"></i>
-                    <p>Django</p>
-                </div>
-                <div>
-                <i class="fa-solid fa-floppy-disk"></i>
-                    <p4>PHP 5.5</p4>
-                </div>
-                <div>
-                    <i class="fa-solid fa-paint-roller"></i>
-                    <p>Design de Logos</p>
-                </div>
-                <div>
-                    <i class="fa-solid fa-palette"></i>
-                    <p>Práticas com Canva</p>
-                </div>
-                <div>
-                    <i class="fa-solid fa-signature"></i>
-                    <p>Empreendorismo</p>
-                </div>
-                <div>
-                    <i class="fa-solid fa-shirt"></i>
-                    <p>Character Design</p>
-                </div>
-                <div>
-                    <i class="fa-solid fa-earth-americas"></i>
-                    <p>World Builder</p>
-                </div>
-                <div class="text-secondary">
-                    <i class="fa-solid fa-robot"></i>
-                    <p>IA com Python</p>
-                </div>
-                <div class="text-success">
-                    <i class="fa-solid fa-floppy-disk"></i>
-                    <p>ShellScript</p>
-                </div>
-                <div>
-                    <i class="fa-brands fa-git"></i>
-                    <p4>Git</p4>
-                </div>
-                <div>
-                    <i class="fa-brands fa-wordpress"></i>
-                    <p>Wordpress</p>
-                </div>
-                <div>
-                    <i class="fa-regular fa-face-grin-wide"></i>
-                    <p>Pixelart</p>
+                    <br />
+                    <br />
+                    <p class="text-primary">
+                        Outras aptidões:
+                    </p>
+                    <hr />
+                    <div class="grid-box">
+                        <div class="text-success">
+                            <i class="fa-brands fa-node"></i>
+                            <p>Node</p>
+                        </div>
+                        <div style="color: #905beb;">
+                            <i class="fa-brands fa-kickstarter-k"></i>
+                            <p>Kotlin</p>
+                        </div>
+                        <div>
+                            <i class="fa-solid fa-mobile-screen"></i>
+                            <p>Android Studio</p>
+                        </div>
+                        <div class="unity">
+                            <i class="fa-brands fa-unity"></i>
+                            <p>Unity (C#)</p>
+                        </div>
+                        <div>
+                            <i class="fa-solid fa-panorama"></i>
+                            <p>Photoshop</p>
+                        </div>
+                        <div>
+                            <i class="fa-solid fa-film"></i>
+                            <p>Sony Vegas</p>
+                        </div>
+                        <div>
+                            <i class="fa-regular fa-face-grin-wide"></i>
+                            <p>Desenho Anime</p>
+                        </div>
+                    </div>
+                    <br />
+                    <br />
+                    <p class="text-primary">
+                        Aptidões Específicas ou pessoais:
+                    </p>
+                    <hr />
+                    <div class="grid-box">
+                        <div class="text-danger">
+                            <i class="fa-solid fa-s"></i>
+                            <p><span class="text-primary">Es</span><span class="text-white">p</span><span class="text-primary">an</span><span class="text-white">hol</span> Fluente</p>
+                        </div>
+                        <div class="text-info">
+                            <i class="fa-solid fa-flag-usa"></i>
+                            <p>Inglês Avançado</p>
+                        </div>
+                        <div>
+                            <i class="fa-brands fa-java"></i>
+                            <p>Java</p>
+                        </div>
+                        <div class="text-warning">
+                            <i class="fa-solid fa-bolt"></i>
+                            <p>Vite</p>
+                        </div>
+                        <div>
+                            <i class="fa-solid fa-hard-drive"></i>
+                            <p style="font-size:8px">Manutenção Computadores</p>
+                        </div>
+                        <div>
+                            <i class="fa-solid fa-person-chalkboard"></i>
+                            <p>Monitor de Programação</p>
+                        </div>
+                        <div>
+                            <i class="fa-solid fa-tree"></i>
+                            <p>Django</p>
+                        </div>
+                        <div>
+                        <i class="fa-solid fa-floppy-disk"></i>
+                            <p>PHP 5.5</p>
+                        </div>
+                        <div>
+                            <i class="fa-solid fa-paint-roller"></i>
+                            <p>Design de Logos</p>
+                        </div>
+                        <div>
+                            <i class="fa-solid fa-palette"></i>
+                            <p>Práticas com Canva</p>
+                        </div>
+                        <div>
+                            <i class="fa-solid fa-signature"></i>
+                            <p style="font-size: 7px">Empreendorismo</p>
+                        </div>
+                        <div>
+                            <i class="fa-solid fa-shirt"></i>
+                            <p>Character Design</p>
+                        </div>
+                        <div>
+                            <i class="fa-solid fa-earth-americas"></i>
+                            <p>World Builder</p>
+                        </div>
+                        <div class="text-secondary">
+                            <i class="fa-solid fa-robot"></i>
+                            <p>IA com Python</p>
+                        </div>
+                        <div class="text-success">
+                            <i class="fa-solid fa-floppy-disk"></i>
+                            <p>ShellScript</p>
+                        </div>
+                        <div>
+                            <i class="fa-brands fa-git"></i>
+                            <p>Git</p>
+                        </div>
+                        <div>
+                            <i class="fa-brands fa-wordpress"></i>
+                            <p>Wordpress</p>
+                        </div>
+                        <div>
+                            <i class="fa-regular fa-face-grin-wide"></i>
+                            <p>Pixelart</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
